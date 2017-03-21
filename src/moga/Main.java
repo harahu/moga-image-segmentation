8fd666da1129c35a7bfdb2063b87934e8d59065d
Hans Olav Slotte <Hans Olav Slotte>
2017-03-21 15:32:10 +0100

    Able to create mst

--- src/moga/Main.java
+++ src/moga/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
         System.out.println("("+Integer.toString(img.getWidth())+","+Integer.toString(img.getHeight())+")");
 
-        int[] mst = generateMST(img.getWidth(), img.getHeight(), img);
+        int[] mst = generateMST(img.getWidth(), img.getHeight(), img, new Random());
     }
 
     public static BufferedImage getImage(String pathName) {
@@ -88,15 +88,79 @@ public class Main {
         return edges;
     }
 
-    public static int[] generateMST(int x_sz, int y_sz, BufferedImage img) {
-        int sz = x_sz*y_sz;
-        int[] directed_mst = new int[sz];
-        Arrays.fill(directed_mst, -1);
+    public static int getEdgeCost(int a, int b, int x_sz, BufferedImage img) {
+        Color c_0 = new Color(img.getRGB(a%x_sz, a/x_sz));
+        Color c_1 = new Color(img.getRGB(b%x_sz, b/x_sz));
 
-        ArrayList<EdgeCost> edges = getEdgeCosts(x_sz, y_sz, img);
+        double r_dist = c_0.getRed()-c_1.getRed();
+        double g_dist = c_0.getGreen()-c_1.getGreen();
+        double b_dist = c_0.getBlue()-c_1.getBlue();
+
+        return (int)Math.pow(r_dist, 2) + (int)Math.pow(g_dist, 2) + (int)Math.pow(b_dist, 2);
+    }
+
+    public static ArrayList<EdgeCost> getEdges(int a, int x_sz, int y_sz, BufferedImage img) {
+        int sz = x_sz*y_sz;
+        ArrayList<EdgeCost> edges = new ArrayList<>();
+
+        if((a%x_sz)-1 >= 0) edges.add(new EdgeCost(a, a-1, getEdgeCost(a, a-1, x_sz, img)));
+        if((a%x_sz)+1 < x_sz) edges.add(new EdgeCost(a, a+1, getEdgeCost(a, a+1, x_sz, img)));
+        if(a-x_sz >= 0) edges.add(new EdgeCost(a, a-x_sz, getEdgeCost(a, a-x_sz, x_sz, img)));
+        if(a+x_sz < sz) edges.add(new EdgeCost(a, a+x_sz, getEdgeCost(a, a+x_sz, x_sz, img)));
+
+        return edges;
+    }
+
+    public static ArrayList<EdgeCost> generateMST(int x_sz, int y_sz, BufferedImage img, Random randomizer) {
+        int sz = x_sz*y_sz;
+
+        int a;
+
+        a = (int)((double)sz * randomizer.nextDouble());
+        ArrayList<EdgeCost> edges = getEdges(a, x_sz, y_sz, img);
 
         Collections.sort(edges);
 
-        return directed_mst;
+        ArrayList<EdgeCost> mst = new ArrayList<>();
+        ArrayList<Integer> nodes_in_mst = new ArrayList<>();
+        nodes_in_mst.add(a);
+
+        int count = 0;
+        while(mst.size() < sz-1 && !edges.isEmpty()) {
+            if(count % 1000 == 0) {
+                System.out.println(count);
+            }
+            count++;
+            EdgeCost edge = edges.remove(0);
+            mst.add(edge);
+            nodes_in_mst.add(edge.getB());
+
+            ArrayList<Integer> remove_by_index = new ArrayList<>();
+
+            for(int i = 0; i < edges.size(); ++i) {
+                if(edges.get(i).getB() == edge.getB()) {
+                    remove_by_index.add(i);
+                }
+            }
+
+            for(int i = 0; i < remove_by_index.size(); ++i) {
+                edges.remove(remove_by_index.get(i).intValue());
+                for(int j = i; j < remove_by_index.size(); ++j) {
+                    remove_by_index.set(j, remove_by_index.get(j)-1);
+                }
+            }
+
+            ArrayList<EdgeCost> new_edges = getEdges(edge.getB(), x_sz, y_sz, img);
+
+            for(int i = 0; i < new_edges.size(); ++i) {
+                if(!nodes_in_mst.contains(new_edges.get(i).getB())) {
+                    edges.add(new_edges.get(i));
+                }
+            }
+
+            Collections.sort(edges);
+        }
+
+        return mst;
     }
 }
